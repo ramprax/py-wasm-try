38b267e992fdc783255a6ebf8ef219eebff57e58
Ramprakash R <ramprax@gmail.com>
2021-03-04 01:57:07 +0530

    Add download link for wordcloud image

--- js/wc_gen.js
+++ js/wc_gen.js
@@ -17,17 +17,18 @@ let do_wc_gen = function(input_text_value) {
   return pyodide.runPython('wc_gen(\'\'\''+input_text_value+'\'\'\')');
 }
 
-let wcGenTextHandler = function(wc_gen_button, input_text_element, output_image) {
+let wcGenTextHandler = function(wc_gen_button, input_text_element, output_image, dl_link) {
   return function() {
     wc_gen_button.disabled = true;
     let b64img_str = do_wc_gen(input_text_element.value);
     output_image.src = 'data:image/png;base64,' + b64img_str;
+    dl_link.href = 'data:image/png;base64,' + b64img_str;
     wc_gen_button.disabled = false;
     return false;
   }
 }
 
-let wcGenFileHandler = function(wc_gen_button, input_file_element, output_image) {
+let wcGenFileHandler = function(wc_gen_button, input_file_element, output_image, dl_link) {
   return function() {
     if(input_file_element.files[0]) {
         wc_gen_button.disabled = true;
@@ -38,6 +39,7 @@ let wcGenFileHandler = function(wc_gen_button, input_file_element, output_image)
             let b64img_str = do_wc_gen(fileString);
 
             output_image.src = 'data:image/png;base64,' + b64img_str;
+            dl_link.href = 'data:image/png;base64,' + b64img_str;
             wc_gen_button.disabled = false;
         }
         fr.onprogress = console.log;
@@ -63,13 +65,15 @@ languagePluginLoader.then(() => {
   let wc_gen_text_button = document.getElementById('wc_gen_text');
   let input_text_element = document.getElementById('input_text');
   let output_text_image = document.getElementById('output_text_image');
-  wc_gen_text_button.onclick = wcGenTextHandler(wc_gen_text_button, input_text_element, output_text_image);
+  let text_img_dl_link = document.getElementById('text_image_dl');
+  wc_gen_text_button.onclick = wcGenTextHandler(wc_gen_text_button, input_text_element, output_text_image, text_img_dl_link);
   wc_gen_text_button.disabled = false;
 
   let wc_gen_file_button = document.getElementById('wc_gen_file');
   let input_file_element = document.getElementById('input_file');
   let output_file_image = document.getElementById('output_file_image');
-  wc_gen_file_button.onclick = wcGenFileHandler(wc_gen_file_button, input_file_element, output_file_image);
+  let file_img_dl_link = document.getElementById('file_image_dl');
+  wc_gen_file_button.onclick = wcGenFileHandler(wc_gen_file_button, input_file_element, output_file_image, file_img_dl_link);
   wc_gen_file_button.disabled = false;
 })
 .catch(console.log);
